5c07537fcedc6389949703b48ab855ee36c4e924
dami0806 <dami0804@naver.com>
2022-12-30 21:26:02 +0900

    Networkingfile

--- MusicApp/Controllers/ViewController.swift
+++ MusicApp/Controllers/ViewController.swift
@@ -15,7 +15,42 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    // 네트워크 매니저 (싱글톤)
+    var networkManager = NetworkManager.shared
+    
+    // (음악 데이터를 다루기 위함) 빈배열로 시작
+    
+    
+    
+    
+    
+    // 테이블뷰 셋팅
+    func setupTableView() {
+        
+        
+    }
+    
+    // 데이터 셋업
+    func setupDatas() {
+        
+        
+    }
 
+}
+
+
+
+extension ViewController: UITableViewDelegate {
+    // 테이블뷰 셀의 높이를 유동적으로 조절하고 싶다면 구현할 수 있는 메서드
+    // (musicTableView.rowHeight = 120 대신에 사용가능)
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 120
+//    }
+    
+    //높이가 유동적이고 더 정확함
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
 
 }
 
--- MusicApp/Helper/constants.swift
+++ MusicApp/Helper/constants.swift
@@ -9,6 +9,11 @@ import UIKit
 // MARK: - Name Space
 
 // 사용하게될 API 문자열 묶음
+public enum MusicApi {
+    static let requestUrl = "https://itunes.apple.com/search?"
+    static let mediaParam = "media=music"
+    
+}
 
 // 사용하게될 Cell 문자열 묶음
 
